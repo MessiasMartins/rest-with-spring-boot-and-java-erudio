db1c6a725f0c1461d629aca17be9c2f34a115d5f
Messias Martins <messiasmartins@outlook.com>
2023-11-27 08:43:15 -0300

    Atualizações diversas

---
+++ 12_CustomSerialization/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`,`address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Belo Horizonte', 'Messias', 'Male', 'Martins'),
+	(2, 'Belo Horizonte', 'Marcela', 'female', 'Martins'),
+	(3, 'Belo Horizonte', 'Marcia', 'female', 'Martins'),
+	(4, 'Belo Horizonte', 'Melisa', 'female', 'Martins');
